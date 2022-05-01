b1d4d46e8770d5b035ade4f8b446f46cdb982d59
hardikhacker <64286654+hardikhacker@users.noreply.github.com>
2022-05-01 21:19:56 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import sys
 API_TOKEN = os.environ.get("API_TOKEN", False)
 bot = telebot.TeleBot(API_TOKEN)
 python_path = 'python'
-startjn=[]
+startjn
 print(os.system("$(which python) -V"))
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
@@ -42,7 +42,7 @@ def plugin(message):
 def joinnow(message):
     chat_id = message.chat.id
     Starting_group = message.text
-    startjn.append(Starting_group)
+    startjon=startjn(Starting_group)
     print(Starting_group)
     try:
         subprocess.run([python_path, 'joining.py'])
@@ -50,6 +50,8 @@ def joinnow(message):
     except Exception as e:
         bot.reply_to(message, f'\t!!!ERROR!!! \n\n {e}')
     return(Starting_group)
+def startjn(startingvalue):
+    retun(startingvalue)
 def send(message):
     chat_id = message.chat.id
     previous_message = message.text
